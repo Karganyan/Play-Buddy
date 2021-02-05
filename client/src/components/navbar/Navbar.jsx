90014d9fc067dd502ffd6fe2168a3024360fe127
Timur <kandareli1995@gmail.com>
2021-02-05 11:21:17 +0300

    коммит перед пулом

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import { Link } from "react-router-dom"
 import { useHistory } from "react-router"
+import { useEffect, useState } from "react"
 
 const Navbar = () => {
+  const [ user, setUser ] = useState({})
   const history = useHistory()
+  useEffect(() => {
+    (async () => {
+      const req = await fetch('http://localhost:3001/auth/in-session', {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-type': 'application/json',
+        }
+      })
+      const res = await req.json()
+      setUser(res)
+    })()
+  }, [])
+  console.log(user)
   const logoutHandler = async () => {
     const req = await fetch('http://localhost:3001/auth/logout', {
       method: 'GET',
@@ -13,24 +29,37 @@ const Navbar = () => {
     })
     if (req.status === 200) {
       // dispatch({type: 'LOGOUT', payload: {}})
+      setUser({})
       history.push('/')
+      console.log(user)
     }
   }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
       <div className="container-fluid ">
         <Link className="navbar-brand" to="/">ABBA</Link>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/signin">Sign In</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/signup">Sign Up</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/logout" onClick={logoutHandler}>Log Out</Link>
-            </li>
+            {user.id
+              ? <>
+                <li className="nav-item">
+                  <a className="nav-link">{user.name}</a>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/logout" onClick={logoutHandler}>Log Out</Link>
+                </li>
+              </> :
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signin">Sign In</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signup">Sign Up</Link>
+                </li>
+              </>
+            }
+
           </ul>
         </div>
       </div>
