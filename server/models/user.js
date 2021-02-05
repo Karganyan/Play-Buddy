97856ccf7c2b17abea26981d94b3e17ff8b3f2f5
Artem <artem-karganyan@bk.ru>
2021-02-05 11:41:41 +0300

    Дополнение в дб

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,9 @@ const userSchema = new mongoose.Schema({
   password: String,
   email: String,
   fav_games: [{ type: mongoose.Schema.Types.ObjectId, ref: "Game" }],
-  googleID: String
+  googleID: String,
+  userEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+  userChats: [{ type: mongoose.Schema.Types.ObjectId, ref: "Chat" }],
 });
 
 module.exports = mongoose.model("User", userSchema);
