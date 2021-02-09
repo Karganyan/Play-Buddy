334e18a951562092f3b5856ee1e4b7859fe2ecf6
Artem <artem-karganyan@bk.ru>
2021-02-09 14:11:17 +0300

    wsdf

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -5,26 +5,7 @@ import ModalCHat from "../ModalChat/ModalChat";
 import styles from './Chat.module.css'
 
 function Chat() {
-  let [store, setStore] = useState({})
-  useEffect(()=>{
-    (async()=>{
-      await setStore(useSelector(store => {
-        console.log(store);
-        return store
-      }))
-      console.log(store);
-      if (store.userChats?.length<1) {
-        console.log(localStorage.getItem('store'));
-        setStore(localStorage.getItem('store'))
-        console.log(store);
-      } else {
-        localStorage.setItem('store', JSON.stringify(store));
-      }
-    })()
-
-  },[])
-
-
+  const store = useSelector(store => store)
   const dispatch = useDispatch()
   const chatHandler = (id) => {
     dispatch({ type: SET_MODAL_CHAT, payload: id })
