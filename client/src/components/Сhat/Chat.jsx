5b5dca4d321552ee10146c1da6498531700bd593
Artem <artem-karganyan@bk.ru>
2021-02-11 22:35:52 +0300

    chat end

--- client/src/components/Сhat/Chat.jsx
+++ client/src/components/Сhat/Chat.jsx
@@ -4,12 +4,15 @@ import { useHistory } from "react-router";
 import { SET_MODAL_CHAT } from "../../redux/types/modalChat";
 import ModalCHat from "../ModalChat/ModalChat";
 import styles from './Chat.module.css'
+import './Chat.css'
+import { Link } from "react-router-dom";
 
 function Chat() {
   const store = useSelector(store => store)
   const dispatch = useDispatch()
   const history = useHistory()
   const chat = store.userChats.find(chat => chat._id === store.modalChat)
+  console.log(chat);
   useEffect(() => {
     !store.user.id ? history.push('/signin') : null
   }, [])
@@ -19,18 +22,21 @@ function Chat() {
   return (
     <>
       <div className=' wrapper' >
+      <Link title='Домой' to='/'>
+              <img src='home.svg' className={styles.hamburger} />
+            </Link>
         <div className={styles.headCont}>
           <h2>ЧАТЫ СОБЫТИЙ</h2>
-          {/* <h3 className='titles'>{chat.eventTitle}</h3> */}
+          <h3 className='titles'>{chat && chat.eventTitle}</h3>
         </div>
         <div className={styles.chatsConteiner}>
           {store.userChats.length
             ?
-            store.userChats.map(item => (
-              <div className={styles.currentChat} key={item._id} onClick={() => chatHandler(item._id)}>
-                {item.eventTitle}
-              </div>
-            ))
+            <ul className="list-group">
+              {store.userChats.map(item => (
+                <li className="list-group-item chatsss" key={item._id} onClick={() => chatHandler(item._id)}>{item.eventTitle}</li>
+              ))}
+            </ul>
             :
             'there are no chats here yet'}
         </div>
