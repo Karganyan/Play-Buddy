af09bef5cdd74465c47254008c1e0f09f785feed
Narkas Vanyurkina <nariolimonario@gmail.com>
2021-02-11 21:02:21 +0300

    профиль готов

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -15,88 +15,91 @@ const Profile = () => {
   let avatarPath;
   // console.log(avatar);
 
+  // let avatarPath;
+  if (avatar === '/uploads/avatar.png') {
+    avatarPath = `${avatar}`;
+  } else {
+    avatarPath = `/uploads/${avatar}`;
+  }
 
-    // let avatarPath;
-    if (avatar === '/uploads/avatar.png') {
-      avatarPath = `${avatar}`;
-    } else {
-      avatarPath = `/uploads/${avatar}`;
-    }
+  const history = useHistory();
+  useEffect(() => {
+    !user.id ? history.push('/signin') : null;
+  }, []);
+  // console.log('AVATAR!====>', avatarPath);
 
-    const history = useHistory();
-    useEffect(() => {
-      !user.id ? history.push('/signin') : null;
-    }, []);
-    // console.log('AVATAR!====>', avatarPath);
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  useOnClickOutside(node, () => setOpen(false));
 
-    const [open, setOpen] = useState(false);
-    const node = useRef();
-    useOnClickOutside(node, () => setOpen(false));
+  return (
+    <div className={styles.profile}>
+      <div className={styles.header}>
+        <div className={styles.profileWrapper}>
+          <Link title='Домой' to='/'>
+            <img src='home.svg' className={styles.hamburger} />
+          </Link>
 
-    return (
-      <div className={styles.profile}>
-        <div className={styles.header}>
-          <div className={styles.profileWrapper}>
-            <Link title='Домой' to='/'>
-              <img src='home.svg' className={styles.hamburger} />
-            </Link>
+          <img src={avatarPath} alt='avatar' className={styles.avatar} />
+          <Link to='/edit'>
+            <img title='Настройки' src='settings.svg' className={styles.settings} />
+          </Link>
+        </div>
 
-            <img src={avatarPath} alt='avatar' className={styles.avatar} />
-            <Link to='/edit'>
-              <img title='Настройки' src='settings.svg' className={styles.settings} />
-            </Link>
-          </div>
-
-          <ProfileInfo />
+        <ProfileInfo />
+        <div>
           <>
-            <Link to='/events'>
-              <button
-                className='btn btn-outline-info btn-lg'
-                style={{
-                  width: '200px',
-                  padding: '20px',
-                  fontSize: '25px',
-                  marginRight: '20px',
-                  color: 'white',
-                }}
-              >
-                Мои События
+          <Link to='/events'>
+            <button
+              className='btn btn-outline-info btn-lg'
+              style={{
+                width: '200px',
+                padding: '20px',
+                fontSize: '25px',
+                marginRight: '20px',
+                color: 'white',
+              }}
+            >
+              Мои События
             </button>
-            </Link>
+          </Link>
 
-            <Link to='/chats'>
-              <button
-                className='btn btn-outline-info btn-lg'
-                style={{ width: '200px', padding: '37px', fontSize: '25px', color: 'white' }}
-              >
-                Мои Чаты
+          <Link to='/chats'>
+            <button
+              className='btn btn-outline-info btn-lg'
+              style={{ width: '200px', padding: '38px', fontSize: '25px', color: 'white' }}
+            >
+              Мои Чаты
             </button>
-            </Link>
-          </>
-        </div>
-        {/* </div> */}
-
-        <div className={styles.bottom}>
-          {user.information ? (
-            <div>
-              {' '}
-              <h2>Личная информация:</h2>
-              <br />
-              <h5>{user.information} </h5>
-            </div>
-          ) : null}
-          {user.phone ? (
-            <div>
-              <h3>Со мной можно связаться по номеру</h3> <br />
-              <h5>{user.phone} </h5>
-            </div>
-          ) : null}
-          <br />
-          <br />
-          <h1 style={{ color: '#a9294f', fontSize: "60px", textShadow: '1px 1px 2px #e7e7de' }}>Любимые Игры</h1>
-          {user.fav_games ? <ProfileFavGames /> : <div>Пока не выбрано ни одной любимой игры</div>}
-        </div>
+          </Link>
+        </>
       </div>
-    );
-}
-export default Profile
+
+      <div className={styles.bottom}>
+        {user.information ? (
+          <div>
+            {' '}
+            <h2 style={{ color: '#fcf876' }}>Личная информация</h2>
+            <h3>{user.information} </h3>
+          </div>
+        ) : null}
+        <br />
+        <br />
+        {user.phone ? (
+          <div>
+            <h2 style={{ color: '#fcf876' }}>Со мной можно связаться по номеру</h2> <br />
+            <h3>{user.phone} </h3>
+          </div>
+        ) : null}
+        <br />
+        <br />
+        <h1 style={{ color: '#fcf876', fontSize: '50px', textShadow: '1px 1px 2px #e7e7de' }}>
+          Любимые Игры
+        </h1>
+        {user.fav_games ? <ProfileFavGames /> : <div>Пока не выбрано ни одной любимой игры</div>}
+      </div>
+      </div>
+      </div>
+  );
+};
+export default Profile;
